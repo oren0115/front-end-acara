52cb3ff3e8b711b7adc4741509ead8788aa52757
baginda <nyomana369@gmail.com>
2025-06-07 21:09:41 +0700

    Add initial configuration and components for Next.js application
    - Created .env and .prettierrc files for environment variables and code formatting.
    - Updated package.json and package-lock.json to include new dependencies: @nextui-org/react, axios, clsx, framer-motion, next-auth, and tailwind-merge.
    - Configured Tailwind CSS with NextUI integration in tailwind.config.ts.
    - Added PageHead component for managing document head elements.
    - Implemented Axios instance with authentication support.
    - Updated _app.tsx to wrap the application in NextUIProvider.
    - Created utility function for class name management using clsx and tailwind-merge.
    - Added global styles and initial layout in index.tsx.
    - Introduced environment configuration for API URL.
    - Commented out unused CSS rules in globals.css.
    - Added new utility functions and components to enhance application structure.

---
+++ src/config/environment.ts
@@ -0,0 +1,7 @@
+
+
+const environment = {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+}
+
+export default environment;
---
+++ src/libs/axios/instance.ts
@@ -0,0 +1,43 @@
+import environment from "@/config/environment";
+
+import axios from "axios";
+import { error } from "console";
+import { request } from "http";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+
+
+interface CustomSession extends Session {
+    accessToken?: string;
+
+}
+
+const headers = {
+    "Content-Type": "application/json"
+}
+
+const instance = axios.create({
+    baseURL: environment.API_URL,
+    headers,
+    timeout: 60 * 1000,
+});
+
+
+instance.interceptors.request.use(
+    async(request) =>{
+        const session: CustomSession | null = await getSession();
+        if(session && session.accessToken){
+            request.headers.Authorization = `Bearer ${session.accessToken}`;
+        }
+        return request;
+    },
+    (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject(error),
+);
+
+
+export default instance;
